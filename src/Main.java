5ebe5889b8ba4dc55cf3122e9c05f19da1e9f027
Thiago-Unicodes <ceo.unicodes@gmail.com>
2023-08-18 23:23:34 -0300

    Atualizando o projeto com as pastas do projeto

---
+++ src/Main.java
@@ -0,0 +1,63 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		/*INTEIROS numeros sem quebra de casas decimais*/
+		
+		/*operação básica de matematica*/
+		int nota1 = 90;
+		int nota2 = 70;
+		int nota3 = 50;
+		int nota4 = 88;
+		
+		System.out.println("Media das notas = " + (nota1 + nota2 + nota3 + nota4)/4 );
+		/*---------------------------------------------------------------------------*/
+		
+		/*DOUBLE numeros com casas decimais*/
+		
+		/*operação básica de matematica*/
+		double n1 = 90;
+		double n2 = 70;
+		double n3 = 50;
+		double n4 = 88;
+		
+		System.out.println("Media das notas = " + (n1 + n2 + n3 + n4)/4 );
+		/*---------------------------------------------------------------------------*/
+		
+		/*CHAR representa uma letra !apenas uma letra*/
+		
+		/*no char usamos aspas simples '' */
+		char pessoaFisica = 'F';
+		char pessoaJuridica = 'J';
+		char sexoMasculino = 'M';
+		char sexoFeminino = 'F';
+		
+		/*testando o char*/
+		if(sexoFeminino == 'F') {
+			System.out.println("Feminino!");
+		}else {
+			System.out.println("Masculino");
+		}
+		/*---------------------------------------------------------------------------*/
+		
+		/*STRING é para textos de tamanho qualquer*/
+		
+		/*no string sempre usamos aspas duplas "" */
+		String nome = "Thiago Lima da Silva";
+		String cpf = "082.252.644.10";
+		String tel = "14 98828-0816";
+		String endereco = "Rui Barbosa, 33";
+		
+		String saida = nome + " portador do cpf " + cpf + " e celular " + tel + " mora na rua " + endereco;
+		
+		System.out.println(saida);
+		/*---------------------------------------------------------------------------*/
+		
+		
+		
+		
+		
+	}
+
+}
