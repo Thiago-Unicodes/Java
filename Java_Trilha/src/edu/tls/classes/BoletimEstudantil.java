abaa7b615438ffd2b94228d2a7b6e83ba495f8d1
Thiago-Unicodes <ceo.unicodes@gmail.com>
2023-08-23 21:27:40 -0300

    Subindo o primeiro projeto

---
+++ Java_Trilha/src/edu/tls/classes/BoletimEstudantil.java
@@ -0,0 +1,36 @@
+package edu.tls.classes;
+
+public class BoletimEstudantil {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*
+		 * SEM INDENTAÇÃO
+		 * 
+		int mediaFinal = 6;
+		if(mediaFinal < 6)
+		System.out.println("REPPROVADO");
+		else if(mediaFinal == 6)
+		System.out.println("PROVA MINERVA");
+		else
+		System.out.println("APROVADO");
+		*/
+		
+		int mediaFinal = 5;
+		
+		if(mediaFinal < 6)
+			
+			System.out.println("REPPROVADO");
+		
+		else if(mediaFinal == 6)
+			
+			System.out.println("PROVA MINERVA");
+		
+		else
+			
+			System.out.println("APROVADO");
+
+	}
+
+}
---
+++ Java_Trilha/src/edu/tls/variaveis/MinhasVariaveis.java
@@ -0,0 +1,13 @@
+package edu.tls.variaveis;
+
+public class MinhasVariaveis {
+
+	public static void main(String[] args) {
+		
+		//TIPOS DE VARIAVEIS E CONSTANTES
+		
+		
+
+	}
+
+}
